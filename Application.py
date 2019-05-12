c0cba2d5e0345e2e2d3f0092162ef83c71f6f0a7
gusvs125 <43396597+gusvs125@users.noreply.github.com>
2019-05-11 22:55:55 -0500

    Add files via upload

---
+++ Application.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	 return "Hello, world!"
+
+	
+@app.route('/<usuario>')
+def ruta(usuario):
+	return('<p>Hello , {}.</p>'.format(usuario))
+    	
